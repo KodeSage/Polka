7972df235c77bc75d82904125c4ecf74e3e0f7b3
James Harrison <somtochukwujames2000@gmail.com>
2024-03-10 23:16:27 +0100

    intializations of Polka

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,224 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+  web3FromSource,
+} from "@polkadot/extension-dapp";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { SiwsMessage } from "@talismn/siws";
+import { message, Input } from "antd";
 
-export default function Home() {
+function App() {
+  const [activeExtension, setActiveExtension] = useState<any>(null);
+  const [accountConnected, setAccountConnected] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [transactionLoading, setTransactionLoading] = useState(false);
+  const [signedIn, setSignIn] = useState(false);
+  const [jwtToken, setJwtToken] = useState(null);
+  const [amountToBurn, setAmountToBurn] = useState<any>(0);
+
+  const truncateMiddle = (address: string) => {
+    let midPoint = parseInt((address.length / 2).toString());
+    return (
+      address.slice(0, midPoint - 10) +
+      "........" +
+      address.slice(midPoint + 10, address.length)
+    );
+  };
+
+  const connectExtension = async () => {
+    setLoading(true);
+    try {
+      let activeExtension = await web3Enable("KodeSage Encode x Polkadot  Hackathon");
+      setActiveExtension(activeExtension);
+      console.log(activeExtension);
+
+      let accounts = null;
+      if (activeExtension && activeExtension.length > 0) {
+        accounts = await web3Accounts();
+      } else {
+        message.error("No Account Found");
+        throw new Error("No Account Found");
+      }
+      setAccountConnected(accounts);
+      message.success("Account Connected");
+    } catch (e) {
+      console.log(e);
+      message.error("Connection Error: " + e);
+    }
+    setLoading(false);
+  };
+  const initTransaction = async () => {
+    if (parseInt(amountToBurn) <= 0) {
+      message.error("Enter an amount greater than zero");
+      return;
+    }
+    setTransactionLoading(true);
+    const wsProvider = new WsProvider("wss://westend-rpc.polkadot.io");
+    const api = await ApiPromise.create({ provider: wsProvider });
+
+    const injector = await web3FromAddress(accountConnected[0].address);
+
+    const tx = api.tx.balances.transferKeepAlive(
+      "5GbKMhCgYSVLRy62ZXFZuySSuLQcK7SstXbf9EvZpebkndGZ",
+      parseFloat(amountToBurn)
+    );
+
+    tx.signAndSend(
+      accountConnected[0].address,
+      { signer: injector.signer },
+      ({ status }) => {
+        if (status.isInBlock) {
+          console.log(
+            `Completed transaction at block hash ${status.asInBlock.toString()}`
+          );
+          message.success(
+            `Completed transaction at block hash ${status.asInBlock.toString()}`
+          );
+        } else {
+          console.log(`Current Status: ${status.type}`);
+          message.info(`Current Status: ${status.type}`);
+        }
+        setTransactionLoading(false);
+      }
+    ).catch((error) => {
+      console.log(`:( transaction failed`, error);
+      message.error(`:( transaction failed: ` + error);
+      setTransactionLoading(false);
+    });
+  };
+  const handleSignIn = async () => {
+    setLoading(true);
+    const nonceRes = await fetch("/getnonce");
+    const data = await nonceRes.json();
+    const { nonce } = data;
+    const siws = new SiwsMessage({
+      domain: "localhost",
+      uri: "http://localhost:5173",
+      address: accountConnected[0].address,
+      nonce,
+      statement: "Welcome to my Polka Dapp",
+      chainName: "Polkadot",
+    });
+    try {
+      const injectedExtension = await web3FromSource(
+        accountConnected[0].meta.source
+      );
+      const signed = await siws.sign(injectedExtension);
+      const verifyRes = await fetch("/verify", {
+        method: "POST",
+        body: JSON.stringify({
+          ...signed,
+          address: accountConnected[0].address,
+        }),
+      });
+      const verified = await verifyRes.json();
+      if (verified.error) throw new Error(verified.error);
+      message.success("Sign In Successful");
+      setSignIn(true);
+      setJwtToken(verified.jwtToken);
+    } catch (e) {
+      message.error("Sign Failed: " + e);
+    }
+    setLoading(false);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="nav">
+        <div>
+          <p className="nav-brand">Polka: Onboarding Dapp Mirroring Encode WorkShop</p>
+        </div>
+        <div>
+          {!accountConnected ? (
+            <button
+              className="btn"
+              onClick={connectExtension}
+              disabled={loading}
+            >
+              {loading ? "Connecting" : "Connect"}
+            </button>
+          ) : (
+            <button
+              className="btn btn-danger"
+              onClick={() => setAccountConnected(null)}
+            >
+              Disconnect
+            </button>
+          )}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="content">
+        {!accountConnected ? (
+          <div className="page-center-cover">
+            <h2>Connect Wallet to Use application</h2>
+            <button
+              className="btn"
+              onClick={connectExtension}
+              disabled={loading}
+            >
+              {loading ? "Connecting" : "Connect"}
+            </button>
+          </div>
+        ) : (
+          <>
+            {!signedIn ? (
+              <div className="box">
+                <h2
+                  style={{
+                    textAlign: "center",
+                    marginTop: 10,
+                    marginBottom: 10,
+                  }}
+                >
+                  Sign In with Substrate
+                </h2>
+                <div className="card">
+                  <h3 className="header">Selected Account</h3>
+                  <p className="body">
+                    {truncateMiddle(accountConnected[0].address)}
+                  </p>
+                </div>
+                <button
+                  onClick={handleSignIn}
+                  disabled={loading}
+                  className="btn btn-block"
+                >
+                  {loading ? "Signing in..." : "SignIn"}
+                </button>
+              </div>
+            ) : (
+              <div className="box">
+                <h3 className="text-bold">
+                  Selected Extension: {activeExtension[0].name}
+                </h3>
+                <p style={{ marginTop: 10, marginBottom: 10, color: "gray" }}>
+                  Account: {truncateMiddle(accountConnected[0].address)}{" "}
+                </p>
+                <p>Enter WND Amount to Burn</p>
+                <Input
+                  value={amountToBurn}
+                  onChange={(e) => setAmountToBurn(e.target.value)}
+                  type="number"
+                  style={{ background: "transparent", color: "white" }}
+                  placeholder="Enter WND Amount to burn"
+                />
+                <button
+                  className="btn"
+                  style={{ marginTop: 10 }}
+                  onClick={initTransaction}
+                  disabled={transactionLoading}
+                >
+                  {transactionLoading ? `Burning...` : "Burn WND"}
+                </button>
+              </div>
+            )}
+          </>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
+
+export default App;
